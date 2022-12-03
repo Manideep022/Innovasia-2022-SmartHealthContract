b296ec723e7e807b8fefcdfc0c2df7502c2c45f0
Manideep022 <119662614+Manideep022@users.noreply.github.com>
2022-12-03 17:49:58 +0530

    Add files via upload

---
+++ twelfth.js
@@ -0,0 +1,6 @@
+function mani(){
+    let name = getElementById('doctor_name').value;
+    let phno = getElementById('phone_number').value;
+    let des = getElementById('desgination').value;
+    document.getElementById('demo').innerHTML="name:"+name+'\nphone nummber:'+phno+'\ndesgination:'+des;
+    }
